36e36779f1b5d95ee157dff17bf77d16ab89ecb8
Pedrozle <pedrozle@outlook.com>
2023-11-10 23:55:04 -0400

    remove imagens

--- lib/apps/equacao_details.dart
+++ lib/apps/equacao_details.dart
@@ -14,9 +14,6 @@ class EquacaoDetails extends StatefulWidget {
 class _EquacaoDetails extends State<EquacaoDetails> {
   String title = "";
   String definicao = "";
-  String imgPath1 = "";
-  String imgPath2 = "";
-  String imgPath3 = "";
   String explicacao = "";
 
   @override
@@ -26,47 +23,32 @@ class _EquacaoDetails extends State<EquacaoDetails> {
       case 1:
         title = "Método da bisseção";
         definicao =
-            "O método da bisseção consiste em dividir o intervalo [a, b] ao meio, obtendo os subintervalos [a, m] e [m, b], e considerar como intervalo de busca o subintervalo em que f tem sinais opostos nos extremos. Em seguida  repete-se o procedimento com o subintervalo de interesse. Após um número finito de subdivisões ou encontramos uma solução ou sabemos que a raiz encontra-se em algum subintervalo [ak, bk].";
-        imgPath1 = "assets/img/met_bissecao/metodo_bissecao.png";
-        imgPath2 = "assets/img/met_bissecao/metodo_bissecao_table.png";
-        imgPath3 = "assets/img/met_bissecao/metodo_bissecao_grafico.png";
+            "O método da bisseção consiste em dividir o intervalo [a, b] ao meio, obtendo os subintervalos [a, x] e [x, b], e considerar como intervalo de busca o subintervalo em que f tem sinais opostos nos extremos. Em seguida  repete-se o procedimento com o subintervalo de interesse. Após um número finito de subdivisões ou encontramos uma solução ou sabemos que a raiz encontra-se em algum subintervalo [ak, bk].";
         explicacao =
-            "Se considerarmos k = 1 até 4, a = 0.75, b = 1, critério de parada 0.03 aplicando na função f(x) = x^2 + ln(x), ao aplicarmos estes dados na função da bisseção encontramos a tabela e o gráfico abaixo";
+            "Se considerarmos a equação: ${widget.data.equacao}, a = ${widget.data.tabela[1][1]}, b = ${widget.data.tabela[1][2]}, critério de parada = (b-a)/2 < precisão, ao aplicarmos estes dados na função da bisseção encontramos a tabela e o gráfico abaixo";
         break;
       case 2:
         title = "Método de Newton-Raphson";
         definicao =
             "O método de Newton, desenvolvido por Isaac Newton e Joseph Raphson, tem o objetivo de estimar as raízes de uma função. Para isso, escolhe-se uma aproximação inicial para esta";
-        imgPath1 = "assets/img/met_bissecao/metodo_bissecao.png";
-        imgPath2 = "assets/img/met_bissecao/metodo_bissecao_table.png";
-        imgPath3 = "assets/img/met_bissecao/metodo_bissecao_grafico.png";
         explicacao = "";
         break;
       case 3:
         title = "Método da secante";
         definicao =
             "O método das secantes é um algoritmo de busca de raízes que usa uma sequência de raízes de linhas secantes para aproximar cada vez melhor a raiz de uma função f";
-        imgPath1 = "assets/img/met_bissecao/metodo_bissecao.png";
-        imgPath2 = "assets/img/met_bissecao/metodo_bissecao_table.png";
-        imgPath3 = "assets/img/met_bissecao/metodo_bissecao_grafico.png";
         explicacao = "";
         break;
       case 4:
         title = "Método da falsa posição";
         definicao =
             "O método da posição falsa ou regula falsi é um método numérico usado para resolver equações lineares definidas em um intervalo [a, b], partindo do pressuposto de que haja uma solução em um subintervalo contido em [a, b";
-        imgPath1 = "assets/img/met_bissecao/metodo_bissecao.png";
-        imgPath2 = "assets/img/met_bissecao/metodo_bissecao_table.png";
-        imgPath3 = "assets/img/met_bissecao/metodo_bissecao_grafico.png";
         explicacao = "";
         break;
       case 5:
         title = "Método do ponto fixo";
         definicao =
             "O método do ponto fixo é um método de se calcular pontos fixos de funções.  O ponto fixo p de uma função g(x) é simplesmente um ponto tal que  g(p)=p. Ou sejam é um ponto onde a função g(x) encontra a reta x=y";
-        imgPath1 = "assets/img/met_fixo/met_fixo.jpg";
-        imgPath2 = "assets/img/met_fixo/met_fixo_tabela.jpg";
-        imgPath3 = "assets/img/met_fixo/met_fixo_grafico.jpg";
         explicacao = "";
         break;
     }
@@ -93,6 +75,13 @@ class _EquacaoDetails extends State<EquacaoDetails> {
               padding: const EdgeInsets.only(left: 24, right: 8),
               child: Text(
                 definicao,
+                style: const TextStyle(fontStyle: FontStyle.italic, color: Colors.black54),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                explicacao,
                 style: const TextStyle(fontStyle: FontStyle.italic, color: Colors.black54),
               ),
             ),
@@ -135,13 +124,6 @@ class _EquacaoDetails extends State<EquacaoDetails> {
                     ),
                   ]),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                explicacao,
-                style: const TextStyle(fontStyle: FontStyle.italic, color: Colors.black54),
               ),
             ),
             Padding(
