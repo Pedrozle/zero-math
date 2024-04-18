3f1eb470dc37db97fa3dff3dcc6dc7619bb76192
Pedrozle <pedrozle@outlook.com>
2024-04-18 19:14:03 -0400

    rework

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:zeromath/apps/home_page.dart';
-import 'package:zeromath/themes/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Zer0Math',
-      theme: lightMode,
-      darkTheme: darkMode,
-      home: const HomePage(),
-    );
+    return Container();
   }
 }
